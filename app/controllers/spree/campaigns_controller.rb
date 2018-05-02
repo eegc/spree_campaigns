11f97663c5f6b99131e820c30cf625217e9b3f12
Enrique <engarcia@acid.cl>
2018-05-02 14:29:58 -0300

    Add taxon select and basic campaign view

--- app/controllers/spree/campaigns_controller.rb
+++ app/controllers/spree/campaigns_controller.rb
@@ -2,15 +2,15 @@ module Spree
   class CampaignsController < Spree::StoreController
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
     rescue_from ActionView::MissingTemplate, with: :render_404
+    helper 'spree/products'
 
     def show
       @campaign = Spree::Campaign.available.friendly.find(params[:id])
       if @campaign && @campaign.taxon
         @taxon = @campaign.taxon
-        redirect_to seo_url(@taxon)
+        @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
+        @products = @searcher.retrieve_products
       end
-
-      return
     end
   end
 end
